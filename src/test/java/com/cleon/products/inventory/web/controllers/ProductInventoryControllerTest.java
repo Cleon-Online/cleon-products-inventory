836d2e3a5c39dbca5a35d0683e5ecb161806439e
Balaji Varadharajan <vbalaji215@gmail.com>
2020-04-11 23:10:23 -0400

    Updated test class with MockBean for IProductInventoryService

--- src/test/java/com/cleon/products/inventory/web/controllers/ProductInventoryControllerTest.java
+++ src/test/java/com/cleon/products/inventory/web/controllers/ProductInventoryControllerTest.java
@@ -1,10 +1,12 @@
 package com.cleon.products.inventory.web.controllers;
 
+import com.cleon.products.inventory.services.IProductInventoryService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +16,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ProductInventoryController.class)
 class ProductInventoryControllerTest {
+
+    @MockBean
+    IProductInventoryService productInventoryService;
 
     @Autowired
     MockMvc mockMvc;

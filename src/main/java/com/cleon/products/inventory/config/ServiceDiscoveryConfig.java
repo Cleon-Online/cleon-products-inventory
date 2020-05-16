d3024715db6fa32549fa24ee4e786ceead4182df
Balaji Varadharajan <vbalaji215@gmail.com>
2020-05-16 17:33:23 -0400

    Created ServiceDiscoveryConfig.java to register with Eureka discovery service. Closes #6

---
+++ src/main/java/com/cleon/products/inventory/config/ServiceDiscoveryConfig.java
@@ -0,0 +1,17 @@
+package com.cleon.products.inventory.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: Balaji Varadharajan
+ * Class/Interface/Enum Name: ServiceDiscoveryConfig
+ * Inside the package - com.cleon.products.inventory.config
+ * Created Date: 5/16/2020
+ * Created Time: 10:45 AM
+ **/
+@EnableDiscoveryClient
+@Configuration
+public class ServiceDiscoveryConfig {
+}
